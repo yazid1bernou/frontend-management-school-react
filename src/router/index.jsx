3c5bd023aea24ebdeaa67c95a726148a071e7b35
unknown <yazid1bernou@gmail.com>
2024-02-13 10:45:40 +0000

    create router and pages structure

---
+++ src/router/index.jsx
@@ -0,0 +1,27 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../pages/Home" ;
+import Login from "../pages/Login" ;
+import Register from "../pages/Register" ;
+import Users from "../pages/Users" ;
+
+export const router = createBrowserRouter(
+    [
+        {
+            path : '/',
+            element : <Home />
+        },
+        {
+            path : '/login',
+            element : <Login />
+        },
+        {
+            path : '/register',
+            element : <Register />
+        },
+        {
+            path : '/users',
+            element : <Users />
+        },
+
+    ]
+)
